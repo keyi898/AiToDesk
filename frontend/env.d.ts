fa756016ef300fb3bc0af1a4b3532b23164bd5d2
keyi898 <keyi8988@gmail.com>
2026-05-05 11:46:33 +0800

    fix: add Vue global type declarations for vue-tsc

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -4,3 +4,12 @@ declare module 'markdown-it-mark'
 declare module 'markdown-it-deflist'
 declare module 'markdown-it-mathjax'
 declare module '@wekanteam/markdown-it-mermaid'
+
+declare global {
+  namespace Vue {
+    type Ref<T> = import('vue').Ref<T>
+    type ComputedRef<T> = import('vue').ComputedRef<T>
+    type WatchOptions = import('vue').WatchOptions
+    type WatchSource<T> = import('vue').WatchSource<T>
+  }
+}
