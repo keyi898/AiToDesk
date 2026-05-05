484937769c6f98827b6f45336da2f5829ca400f8
keyi898 <keyi8988@gmail.com>
2026-05-05 15:54:35 +0800

    fix: remove AiToDesk from logo, change to aitodesk

--- frontend/src/views/Sider/index.vue
+++ frontend/src/views/Sider/index.vue
@@ -4,7 +4,7 @@
         <div class="logo">
             <div class=logo-left>
                 <n-image :src="logo" object-fit="cover" class="h-30" preview-disabled />
-                <span class="text-[var(--bt-tit-color-secondary)]">AiToDesk</span>
+                <span class="text-[var(--bt-tit-color-secondary)]">aitodesk</span>
             </div>
             <div>
                 <i class="i-common:fold w-18 h-18  cursor-pointer" @click="doFold"></i>
